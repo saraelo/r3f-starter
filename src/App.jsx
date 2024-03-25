06f98952a80e3fb0fcb711c54ec9fd1dc5c66762
Sara Loera <saraloera11@icloud.com>
2024-03-25 10:04:39 -0700

    Add orbit controls and create README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         position: [3, 3, 3],
       }}
     >
+      <OrbitControls />
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />
