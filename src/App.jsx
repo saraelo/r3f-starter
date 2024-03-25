6249016b4ce1fe88476ee845f84210d7c0720604
Sara Loera <saraelo@Saras-MacBook-Pro.local>
2024-03-25 09:09:01 -0700

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <Canvas
+      camera={{
+        position: [3, 3, 3],
+      }}
+    >
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </Canvas>
+  );
+}
+
+export default App;
